726fc6bd64cc5703e198cb4c2ae23e7b5339c12c
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-09 23:36:12 +0100

    service details page created for specific service

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000 ;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middlewARE
 
@@ -26,12 +26,30 @@ const client = new MongoClient(uri, {
     try {
       // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
+        const serviceCollection = client.db('carDoctor').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find()
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/services/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const options = {
+                projection : {title: 1, price: 1, service_id: 1 },
+            };
+            const result = await serviceCollection.findOne(query, options);
+            res.send(result);
+        })
+
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
       // Ensures that the client will close when you finish/error
-      await client.close();
+    //   await client.close();
     }
   }
   run().catch(console.dir);
@@ -39,6 +57,7 @@ const client = new MongoClient(uri, {
 app.get('/', (req, res) =>{
     res.send('car is running')
 })
+
 
 app.listen(port, ()=>{
     console.log(`Car garaze server is running on port ${port}`);
