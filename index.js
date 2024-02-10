6ef3247c0946e297ec8bda789630c035699826ed
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-10 22:13:09 +0100

    img route added for projection

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const client = new MongoClient(uri, {
             const id = req.params.id;
             const query = {_id: new ObjectId(id)}
             const options = {
-                projection : {title: 1, price: 1, service_id: 1 },
+                projection : {title: 1, price: 1, service_id: 1, img: 1 },
             };
             const result = await serviceCollection.findOne(query, options);
             res.send(result);
